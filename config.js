89e35ee6f24db461fea42a03d8aa3b2fb25b43bd
João Duarte <joaocintraduarte@gmail.com>
2026-03-23 14:07:49 -0300

    Mudança na chave pix

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ const CONFIG = {
     whatsappClube: "5519983484323",
     linkDriveGaleria: "https://drive.google.com/drive/folders/seu-link-aqui",
     linkInstagram: "https://instagram.com/clubejovensdorei",
-    chavePix: "f9e35de6-a4ab-4a8b-8309-ef882688d40d"
+    chavePix: "19983484323",
 };
